d3ba766e64f0cabb3cae18096855effceaba51b4
Bruce Lau <liulqx@gmail.com>
2025-11-27 00:09:23 +0800

    feat: wasm golang

---
+++ golang/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "syscall/js"
+
+func add(this js.Value, args []js.Value) interface{} {
+    return args[0].Int() + args[1].Int()
+}
+
+func main() {
+    js.Global().Set("add", js.FuncOf(add))
+
+    // 阻塞防止 main 退出
+    select {}
+}
